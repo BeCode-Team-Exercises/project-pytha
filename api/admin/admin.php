dace89e7c445b2f259daa9f96d8a2fc6b8b686dc
Stijn <stijn.peeters.it@gmail.com>
2020-01-09 17:11:11 +0100

    add test work

--- api/admin/admin.php
+++ api/admin/admin.php
@@ -5,19 +5,29 @@ $isAllowed = ['192.168.139.192' => 'jasper', '192.168.139.177' => 'reinaert', '1
 $clientIP = $_SERVER['REMOTE_ADDR'];
 // check if user is allowed and execute code if allowed
 if (array_key_exists($clientIP, $isAllowed)) {
-    if (array_key_exists('password', $_POST) || !$_POST['password'] === '') {
-        echo $_POST['password'];
+    if (array_key_exists('password', $_POST)) {
+        if (!$_POST['password'] == '') {
+            echo $_POST['password'];
 
-
-//         $dataBase = openConnection()->prepare("SELECT details
-// FROM api.admin;");
-//         foreach (array_keys($_POST) as $item) {
-//             $dataBase->bindParam($item, $_POST[$item]);
-//         };
-//         $dataBase->execute();
-//     }
+            $pdo = new PDO('mysql:host=192.168.139.125;dbname=api;port=3306', 'user', 'user');
+            $test = $pdo->query("SELECT id, `user`, pass FROM api.login WHERE id=3;");
+            if ($test->rowCount() > 0) {
+                while ($row = $test->fetch(PDO::FETCH_ASSOC)) {
+                    var_dump($row);
+                }
+            }
+            // $dataBase = $pdo->prepare("SELECT id, `user`, pass FROM api.login WHERE id=3;");
+            // foreach (array_keys($_POST) as $item) {
+            //     $dataBase->bindParam($item, $_POST[$item]);
+            // };
+            // $dataBase->execute();
+            // $dataBase->fetch(PDO::FETCH_ASSOC);
+            // echo ($dataBase);
+        } else {
+            echo "user is $isAllowed[$clientIP] with ip $clientIP and forgot to enter password?";
+        }
+    }
     // message to clarify that the user did not enter a password
-    echo "user is $isAllowed[$clientIP] with ip $clientIP and forgot to enter password?";
 }
 // if user is not allowed kill the script
 else {
