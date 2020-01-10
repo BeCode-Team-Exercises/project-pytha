dac880248170f11acb015e875f9645bbe7b8c0a9
jan <declercqjan@gmail.com>
2020-01-10 15:52:09 +0100

    added calculation of actual price

--- webshop/src/ProductCard.js
+++ webshop/src/ProductCard.js
@@ -6,15 +6,17 @@ import "./css/App.css";
 class ProductCard extends Component {
   render() {
     const { product_info } = this.props;
+
+    const actual_price =
+      product_info.price_per_unit + (product_info.price_per_unit * (product_info.tax_percentage / 100));
+
     return (
       <div className="card col-md-3">
         <img className="card-img-top" alt="" />
         <div className="card-body">
           <h5 className="card-title">{product_info.name}</h5>
-          <p className="card-text">
-          {product_info.description}
-          </p>
-          <p>{`${product_info.price_per_unit} euro`}</p>
+          <p className="card-text">{product_info.description}</p>
+          <p>{`${actual_price} euro`}</p>
           <a href="" className="btn btn-primary">
             Go somewhere
           </a>
